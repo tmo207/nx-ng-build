2586be7cf69fd61a7fcf1a366e7e091634d1df1b
tmo207 <timojancke@web.de>
2022-01-10 11:56:39 +0100

    init commit

--- libs/ng-lib/jest.config.js
+++ libs/ng-lib/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'nx-ng-bug',
+  displayName: 'ng-lib',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/nx-ng-bug',
+  coverageDirectory: '../../coverage/libs/ng-lib',
   transform: {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular',
   },
